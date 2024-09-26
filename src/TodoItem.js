9b00131db429f0a3db372fc38510c83cfe68ad9a
Tomas Patricio Arriola <tarriola@ledesma.com.ar>
2024-09-25 21:26:04 -0300

    Commit 1

---
+++ src/TodoItem.js
@@ -0,0 +1,10 @@
+function TodoItem(props){
+        return (
+          <li>
+            <span>V</span>
+            <p>Llorar con la llorona</p>
+            <span>X</span>
+          </li>
+      )};
+
+export {TodoItem};
